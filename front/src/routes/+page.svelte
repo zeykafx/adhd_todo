7ba70797f1863e3a94348d181fbf0f651c12d920
Corentin Detry <corentin.detry@gmail.com>
2023-11-03 22:53:58 +0100

    initial code

---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import toast, {Toaster} from "svelte-french-toast";
+    import {AppBar, AppShell} from '@skeletonlabs/skeleton';
+
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+    import {onMount} from "svelte";
+
+    // onMount(() => {
+    //     toast("Hello world!");
+    // });
+</script>
+
+<AppShell>
+    <svelte:fragment slot="header">
+        <AppBar>
+            <svelte:fragment slot="lead">
+                <h1 class="h1 text-2xl font-bold">Magic Todo</h1>
+            </svelte:fragment>
+
+            <svelte:fragment slot="trail">
+                <LightSwitch/>
+            </svelte:fragment>
+        </AppBar>
+    </svelte:fragment>
+
+    <svelte:fragment>
+        <h2>content</h2>
+        <button class="btn variant-soft-primary" on:click={() => toast.success("Hello world")}>Show toast</button>
+    </svelte:fragment>
+
+</AppShell>
+
+<Toaster/>
+
